85c92b75f46b882d6b792ceb6c8a21b94fd9c979
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-10-26 05:52:55 +0700

    init

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Arifikhsan\LaravelCalculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Arifikhsan\LaravelCalculator\CalculatorController');
+        $this->loadViewsFrom(__DIR__.'/views', 'calculator');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
